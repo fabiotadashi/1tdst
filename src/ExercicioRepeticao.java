13a7bfd44418d88eb0802100e59920ad0adae75c
Fabio Tadashi Miyasato <miyasato.fabio@gmail.com>
2020-04-12 19:27:54 -0300

    exercicio repeticao

---
+++ src/ExercicioRepeticao.java
@@ -0,0 +1,16 @@
+public class ExercicioRepeticao {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+
+            for (int j = 0; j < i; j++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+    }
+
+}
